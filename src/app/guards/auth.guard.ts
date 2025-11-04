2621434f1a3ee2b46152df8e0a85c57307bd3cb3
TUWINDI <hosting@tuwindi.org>
2025-11-04 17:41:24 +0000

    auth guard added

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,27 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivate, CanActivateFn, Router, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+
+export class AuthGuard implements CanActivate {
+  
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  canActivate():
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    
+    // Vérifie si l'utilisateur est authentifié
+    if (this.authService.isAuthenticated()) {
+      return true;
+    }
+    // Sinon, redirige vers la page de connexion
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
